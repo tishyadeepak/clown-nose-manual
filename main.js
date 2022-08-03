f53b43d5d14733dac4b8afe85bde07ef8f6548d1
tishyadeepak <91376226+tishyadeepak@users.noreply.github.com>
2022-08-03 17:30:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX  =0;
+noseY  =0;
+
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    fill(255,0,0);
+    storke(255,0,0);
+    circle(noseX,noseY,20);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY =results[0].pose.nose.y;
+        console.log("nose x = " + noseX );
+        console.log("nose y = " + noseY);
+    }
+}
